7c1615106fca266b777c38636015bdd645d10816
1064517852@qq.com <w1994z0115x>
2020-12-18 14:28:32 +0800

    springcloud2020RC 仅加入k8s依赖  等待2020GA

---
+++ noxus-draven-cloud-ilford-rc/noxus-draven-ilford-rc-kubernetes/src/main/java/noxus/draven/ilford/rc/K8sApplication.java
@@ -0,0 +1,19 @@
+package noxus.draven.ilford.rc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 〈功能概述〉<br>
+ *
+ * @className: K8sApplication
+ * @package: noxus.draven.ilford.rc
+ * @author: draven
+ * @date: 2020/12/18 14:27
+ */
+@SpringBootApplication
+public class K8sApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(K8sApplication.class, args);
+    }
+}
